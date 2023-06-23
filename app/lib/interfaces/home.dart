a5f2fefa3e9d3d22f1e7a86e1cc7b2a83334d0de
Sathvik Narala <sathviknarala2003@gmail.com>
2023-06-23 22:17:14 +0530

    small update in UI

--- app/lib/interfaces/home.dart
+++ app/lib/interfaces/home.dart
@@ -44,41 +44,35 @@ class _AppState extends State{
       ):
       Padding(
         padding: const EdgeInsets.all(18.0),
-        child: Column(
-          children: [
-            Expanded(
-              child: GridView.builder(
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 12,
-                  mainAxisSpacing: 12
-                ),
-                itemCount: flow.data.length,
-                itemBuilder: (context, index){
-                  return InkWell(
-                    onTap: (){
-                      context.read<Dataflow>().gridtap(index);
-                      Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (context) => const Display()
-                        )
-                      );
-                    },
-                    child: GridTile(
-                      footer: GridTileBar(
-                          backgroundColor: Colors.black45,
-                          title: Text(flow.data[index].name),
-                          subtitle: Text(flow.data[index].category),
-                        ),
-                      child: Image(
-                        image: NetworkImage(flow.data[index].image),
-                      )
-                    )
-                  );
-                }
-              ),
-            ),
-          ]
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 12,
+            mainAxisSpacing: 12
+          ),
+          itemCount: flow.data.length,
+          itemBuilder: (context, index){
+            return InkWell(
+              onTap: (){
+                context.read<Dataflow>().gridtap(index);
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const Display()
+                  )
+                );
+              },
+              child: GridTile(
+                footer: GridTileBar(
+                    backgroundColor: Colors.black45,
+                    title: Text(flow.data[index].name),
+                    subtitle: Text(flow.data[index].category),
+                  ),
+                child: Image(
+                  image: NetworkImage(flow.data[index].image),
+                )
+              )
+            );
+          }
         ),
       ),
       
